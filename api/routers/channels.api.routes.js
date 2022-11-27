b6a21f686f1b7a445ee0d9a56de50ab60b77a220
LGRosi <lrosi1996@gmail.com>
2022-11-27 16:09:27 -0300

    Inicio de la proteccion de la ruta de channels

--- api/routers/channels.api.routes.js
+++ api/routers/channels.api.routes.js
@@ -1,10 +1,27 @@
 import express from "express";
 import * as channelsApiControllers from "../controllers/channels.api.controllers.js";
+import jwt from "jsonwebtoken";
+
+function isLogin(req, res, next) {
+   const token = req.headers['auth-token'];
+
+   if (!token) {
+      return res.status(401).json({ message: 'No enviaste el token' });
+   }
+   
+   try {
+      const payload = jwt.verify(token, 'CLAVE_SECRETA')
+   } catch (err) {
+      return res.status(401).json({ message: 'Token inválido' });
+   }
+
+   next()
+}
 
 const router = express.Router();
 
 router.route("/")
-   .get(channelsApiControllers.findAll)
+   .get([isLogin], channelsApiControllers.findAll)
    .post(channelsApiControllers.create);
 
 export default router;
