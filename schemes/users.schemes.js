45c2411b113eac791d620cc0112b9f0c2d9a479b
LGRosi <lrosi1996@gmail.com>
2022-11-30 21:10:06 -0300

    Nuevas validaciones en el archivo users schemes

--- schemes/users.schemes.js
+++ schemes/users.schemes.js
@@ -2,12 +2,12 @@ import yup from 'yup';
 
 const userScheme = yup.object({
    name: yup.string().required("El nombre es requerido"),
-   email: yup.string().email("El email no es válido").required("El email es requerido"),
+   email: yup.string().required("El email es requerido").email("El email no es válido"),
    password: yup.string().required("La contraseña es requerida")
 }).noUnknown()
 
 const loginScheme = yup.object({
-   email: yup.string().email("El email no es válido"),
+   email: yup.string().required("El email es requerido").email("El email no es válido"),
    password: yup.string().required("La contraseña es requerida")
 })
 
