91805c3e416de715cc0a4a7d94b252bd2fa6a6a4
LGRosi <lrosi1996@gmail.com>
2023-08-06 19:12:51 -0300

    Se cambio el nombre de la carpeta y archivo de validaciones

--- schema/users.schema.js
+++ schema/users.schema.js
@@ -1,18 +1,18 @@
 import yup from 'yup';
 
-const userScheme = yup.object({
+const userSchema = yup.object({
     name: yup.string().required("El nombre es requerido"),
     email: yup.string().required("El email es requerido").email("El email no es válido"),
     password: yup.string().required("La contraseña es requerida")
 }).noUnknown()
 
-const loginScheme = yup.object({
+const loginSchema = yup.object({
     email: yup.string().required("El email es requerido").email("El email no es válido"),
     password: yup.string().required("La contraseña es requerida")
 })
 
 
 export {
-    userScheme,
-    loginScheme
+    userSchema,
+    loginSchema
 }
